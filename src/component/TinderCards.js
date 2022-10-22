fcb37c7fb89803f4e2a5c280a1cf315270f91177
Subhashree Subhalaxmi <105669273+SubhashreeSS@users.noreply.github.com>
2022-10-22 19:25:21 +0530

    Update TinderCards.js

--- src/component/TinderCards.js
+++ src/component/TinderCards.js
@@ -3,55 +3,11 @@ import TinderCard from 'react-tinder-card';
 import './TinderCards.css'
 
 function TinderCards() {
-  const [cards, setCards] = React.useState([
-    {
-      name: 'Emily',
-      age:'27',
-      job:'Photographer',
-      url:'https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80'
-    },
-    {
-      name:'John',
-      age:'25',
-      job:'Model', 
-      url:'https://photowhoa.com/2015/wp-content/uploads/2016/08/photo-1523913507744-1970fd11e9ff.jpg'
-    },
-    {
-      name:'Jazmine',
-      age:'23',
-      job:'Student',
-      url:'https://i.pinimg.com/564x/87/7b/01/877b01f7092146efb33a6848e786d892.jpg'
-    },
-    {
-      name:'Jack',
-      age:'26',
-      job:'UI/UX developer',
-      url:'https://i.pinimg.com/736x/fa/24/de/fa24decb9fb75a2f35c43e60a2f98e87.jpg'
-    },
-    {
-      name: 'Jessica',
-      age:'26',
-      job:'Marketing Manager',
-      url:'https://cyber-privacy.net/wp-content/uploads/thispersondoesnotexist.com-image01-1024x1024.jpg'
-    },
-    {
-      name:'Aditya',
-      age:'27',
-      job:'Full-Stack Web Developer',
-      url:'https://xsgames.co/randomusers/assets/avatars/male/74.jpg'
-    },
-    {
-      name: 'Shafi',
-      age:'26',
-      job: 'Supply Chain Planner',
-      url:'https://scontent.fblr1-7.fna.fbcdn.net/v/t39.30808-6/312454335_117420991145163_4804843518897472444_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=LncH1m8wc4AAX91H9gl&_nc_ht=scontent.fblr1-7.fna&oh=00_AT-y0DuRroYdrkcrbPpRDbt2eCLr-0FCVw7vLPm81fUaFA&oe=6357A04C'
-    }
 
-  ]);
   return (
     <div className='card-cotainer'>
       {
-        cards.map( card =>(
+        CardData.map( card =>(
           <TinderCard
           className='swipe'
           key={card.name}
