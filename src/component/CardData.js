b44611369b941a991a5894f2d5d104a4643cdda0
Subhashree Subhalaxmi <105669273+SubhashreeSS@users.noreply.github.com>
2023-02-25 20:04:50 +0530

    Add another profile

--- src/component/CardData.js
+++ src/component/CardData.js
@@ -36,10 +36,16 @@ const array=[
     url:'https://xsgames.co/randomusers/assets/avatars/male/74.jpg'
   },
   {
+    name: 'Sakku',
+    age:'23',
+    job: 'Artist',
+    url:'https://scontent.fblr1-4.fna.fbcdn.net/v/t39.30808-6/333246060_586759303076390_6654231637899280834_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=B7esFG-oJm4AX_exUnQ&tn=I5SLAu9x9IJGKA3N&_nc_ht=scontent.fblr1-4.fna&oh=00_AfBuf5dkMWyN8VJkyBwK-JuQ8Rv-xzuWthJPy-6SNFl8SQ&oe=63FE436E'
+  },
+  {
     name: 'Shafi',
     age:'27',
     job: 'Supply Chain Planner',
-    url:'src/component/Shafi.jpg'
+    url:'https://scontent.fblr1-8.fna.fbcdn.net/v/t39.30808-6/301993512_1131255724157683_1949015540227540384_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=Jm4ZcYrWfPoAX_TUQ6U&_nc_ht=scontent.fblr1-8.fna&oh=00_AfAtbRJoZ6f6xicZeZGX4C_vRcRyWZUqXFjPR_Pm1ZMhew&oe=63FE9970'
   },
   {
     name:'Subhadipta',
